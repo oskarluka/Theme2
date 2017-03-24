dc26d6e885b7fc4fadcb5d9057af8eaaeae2a973
oskarluka <oskarluka@hotmail.com>
2017-03-24 12:03:49 +0000

    V0.1.2
    Standard Search Bar

--- header.php
+++ header.php
@@ -13,6 +13,13 @@
 
 		<!-- site header-->
 		<header class="site-header">
+
+		<div class="header-search">
+
+		<?php get_search_form(); ?>
+
+		</div>
+
 			<h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
 			<h5><?php bloginfo('description'); ?>
 
--- search.php
+++ search.php
@@ -2,7 +2,12 @@
 
 get_header();
 
-if (have_posts()) :
+if (have_posts()) :?>
+
+	<h2> Search results for: <?php the_search_query(); ?></h2>
+
+
+	<?php
 	while (have_posts()) : the_post(); ?>
 
 	<?php get_template_part('content'); ?>
