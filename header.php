7e91dcb4873f31ccce0e75883f18d4c764c32c7c
oskarluka <oskarluka@hotmail.com>
2017-03-24 12:33:41 +0000

    V0.1.2

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 	<head>
+		<meta charset="utf-8" />
 		<meta charset="<?php bloginfo('charset'); ?>">
 		<meta name="viewport" content="width=device-width">
 		<title><?php bloginfo('name'); ?></title>
